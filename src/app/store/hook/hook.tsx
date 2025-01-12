9745fb83c27146818a35f20a4c9c1c1ab7795b61
josunkim <32131319+josunkim@users.noreply.github.com>
2025-01-12 22:20:59 +0900

    asd

--- src/app/store/hook/hook.tsx
+++ src/app/store/hook/hook.tsx
@@ -1,7 +1,7 @@
-export const debounce = (func: Function, delay: number) => {
+export const debounce = (func: (...args: unknown[]) => void, delay: number) => {
   let timeout: NodeJS.Timeout;
 
-  return (...args: any[]) => {
+  return (...args: unknown[]) => {
     clearTimeout(timeout); // 이전 타이머 취소
     timeout = setTimeout(() => {
       func(...args); // 지정된 시간 후에 함수 호출
